3a9a2a8918f3400b6cb6bb9b747c928316a553d5
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-03 12:08:59 +0300

    Написание кода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,16 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import java.lang.reflect.Type;
 import java.util.List;
 
 public class Main {
@@ -15,6 +20,8 @@ public class Main {
 
         List<Employee> staff = parseCSV(columnMapping, fileName);
         String json = listToJson(staff);
+
+        writeString(json);
 
     }
 
@@ -32,6 +39,19 @@ public class Main {
     }
 
     public static String listToJson(List<Employee> list) {
-        return null;
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.setPrettyPrinting().create();
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString(String json) {
+        try (FileWriter writer = new FileWriter("src/main/resources/new_data.json")) {
+            writer.write(json);
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
