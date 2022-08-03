12eb5c3585930e5edcc1d192f8721e860214820e
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-03 11:47:12 +0300

    Написание кода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "src/main/resources/data.csv";
+
+        List<Employee> staff = parseCSV(columnMapping, fileName);
+        String json = listToJson(staff);
+
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) throws IOException {
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            reader.close();
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csv.parse();
+        }
+    }
+
+    public static String listToJson(List<Employee> list) {
+        return null;
     }
 }
