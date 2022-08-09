48a93ed1120f83d3fa1439b1c91e3c900b639822
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-09 12:48:15 +0300

    Задача №3.  Пробуем получить  из строчки json массив JSONArray

--- src/main/java/task3/Main.java
+++ src/main/java/task3/Main.java
@@ -2,24 +2,19 @@ package task3;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import task1.Employee;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ParseException {
 
         String json = readString("src/main/resources/new_data.json");
         System.out.println(json);
@@ -35,29 +30,36 @@ public class Main {
         while ((s = bufferedReader.readLine()) != null) {
             sb.append(s);
         }
+        bufferedReader.close();
         return sb.toString();
     }
 
     public static List<Employee> jsonList(String json) throws IOException, ParseException {
         List<Employee> employees = new ArrayList<>();
         JSONParser parser = new JSONParser();
+
+//
+//
         Object obj = parser.parse(json);
 
-//
-//
-        JSONArray jsonArray = new JSONArray();
 
-
+        String data = "[{\"userName\": \"sandeep\",\"age\":30}]  ";
+//        JsonArray jsonArray = new JsonArray();
+//
+//        jsonArray.add(data);
+//
 
         GsonBuilder gsonBuilder = new GsonBuilder();
         Gson gson = gsonBuilder.create();
-        for (Object o : jsonArray) {
-            Employee employee = gson.fromJson(json, Employee.class);
-            System.out.println(employee);
-            employees.add(employee);
-        }
-        Employee employee = gson.fromJson(json, Employee.class);
-        employees.add(employee);
+//        for (Object o : jsonArray) {
+//            Employee employee = gson.fromJson(json, Employee.class);
+//            System.out.println(employee);
+//            employees.add(employee);
+//        }
+
+
+        employees.add(gson.fromJson(data, Employee.class));
+
 
         return employees;
     }
