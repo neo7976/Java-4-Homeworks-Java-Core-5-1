eab799febb2bda05255053f59d86c1f7a0e25981
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-09 12:56:06 +0300

    Задача №3.  Удачное завершение кода

--- src/main/java/task3/Main.java
+++ src/main/java/task3/Main.java
@@ -1,26 +1,24 @@
 package task3;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import com.google.gson.reflect.TypeToken;
 import task1.Employee;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.lang.reflect.Type;
 import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException{
 
         String json = readString("src/main/resources/new_data.json");
-        System.out.println(json);
         List<Employee> list = jsonList(json);
-
-
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
     }
 
     public static String readString(String jsonWay) throws IOException {
@@ -34,35 +32,10 @@ public class Main {
         return sb.toString();
     }
 
-    public static List<Employee> jsonList(String json) throws IOException, ParseException {
-        List<Employee> employees = new ArrayList<>();
-        JSONParser parser = new JSONParser();
-
-//
-//
-        Object obj = parser.parse(json);
-
-
-        String data = "[{\"userName\": \"sandeep\",\"age\":30}]  ";
-//        JsonArray jsonArray = new JsonArray();
-//
-//        jsonArray.add(data);
-//
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        Gson gson = gsonBuilder.create();
-//        for (Object o : jsonArray) {
-//            Employee employee = gson.fromJson(json, Employee.class);
-//            System.out.println(employee);
-//            employees.add(employee);
-//        }
-
-
-        employees.add(gson.fromJson(data, Employee.class));
-
-
-        return employees;
+    public static List<Employee> jsonList(String json){
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<Employee>>() {
+        }.getType();
+        return gson.fromJson(json, type);
     }
-
-
 }
